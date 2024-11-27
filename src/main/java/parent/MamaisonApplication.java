e041bc41bf657ff1a55b8a24002d285874eb9a2d
Jay Rathod <neversettlejay@gmail.com>
2024-11-27 11:30:23 -0500

    Initial commit, feature: added modules: authorization server, resource server, client server
    Signed-off-by: Jay Rathod <neversettlejay@gmail.com>

---
+++ src/main/java/parent/MamaisonApplication.java
@@ -0,0 +1,13 @@
+package parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamaisonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MamaisonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/parent/MamaisonApplicationTests.java
@@ -0,0 +1,13 @@
+package parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MamaisonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
